2125c8924e29c718b9daf4d652377d9e8ce08614
Olga Illarionova <ola.illari@gmail.com>
2020-09-25 22:21:15 -0700

    start working

---
+++ script.js
@@ -0,0 +1,4 @@
+var Questions = [{question: "Q1" , answers :["q1a1","q1a2","q1a3","q1a4"], correct: 3},{question: "Q2" , answers :["q2a1","q2a2","q2a3","q2a4"], correct: 2},{question: "Q3" , answers :["q3a1","q3a2","q3a3","q3a4"], correct: 1},{question: "Q4" , answers :["q4a1","q4a2","q4a3","q4a4"], correct: 3},{question: "Q5" , answers :["q5a1","q5a2","q5a3","q5a4"], correct: 0}];
+var timer = 75;
+var StartBtn = document.querySelector("#start");
+
